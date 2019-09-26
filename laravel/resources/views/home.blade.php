2176b63f0366a00a290195e8f2196e2ea5ef4b86
Reese Morgan <ramorgan212@gmail.com>
2019-09-26 18:21:08 -0400

    ran fin init trying to fix db issues, files got reset, didn't fix db...

--- laravel/resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('content')
-    <h1>Hello World!</h1>
-    <ul>
-        @foreach($tasks as $task)
-            <li>{{ $task }}</li>
-        @endforeach
-    </ul>
-@endsection
