74577e731b359430a2439b4ce6dffde7bcc5fa03
Reese Morgan <ramorgan212@gmail.com>
2019-09-26 11:40:41 -0400

    hardcode tasks and display them on homepage.

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
 @extends('layout')
 @section('content')
     <h1>Hello World!</h1>
+    <ul>
+        <?php foreach ($tasks as $task): ?>
+        <li><?= $task; ?></li>
+        <?php endforeach; ?>
+    </ul>
 @endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,17 +12,24 @@
 */
 
 Route::get('/', function () {
-  return view('welcome');
+
+    $tasks = [
+        "Go to the store",
+        "Go to the market",
+        "Go to work",
+    ];
+
+    return view('welcome', ['tasks' => $tasks]);
 });
 
 Route::get('/old_home', function () {
-  return view('old_welcome');
+    return view('old_welcome');
 });
 
 Route::get('/contact', function () {
-  return view('contact');
+    return view('contact');
 });
 
 Route::get('/about', function () {
-  return view('about');
+    return view('about');
 });
