6e6783bf663949e329a0b602a3f45cba3815154c
Reese Morgan <ramorgan212@gmail.com>
2019-09-27 01:10:07 -0400

    add some styling inline while I wait to be shown front end things :-P

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -5,12 +5,17 @@
 <head>
     <title></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css"/>
+    <style>
+        .is-complete {
+            text-decoration: line-through;
+        }
+    </style>
 </head>
 
-<body>
-    <div class="container">
-        @yield('content')
-    </div>
+<body style="padding-top: 40px;">
+<div class="container">
+    @yield('content')
+</div>
 </body>
 
 </html>
