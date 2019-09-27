a54592258847291c7b73524d9f061c477ae2a301
Reese Morgan <ramorgan212@gmail.com>
2019-09-26 23:36:17 -0400

    add create new project link on project.index

--- laravel/resources/views/Projects/index.blade.php
+++ laravel/resources/views/Projects/index.blade.php
@@ -2,13 +2,14 @@
 
 @section('content')
     <h1>Projects</h1>
-
     @foreach($projects as $project)
         <li>
             <a href="/projects/{{$project->id}}">
                 {{ $project->title }}
             </a>
         </li>
-
     @endforeach
+    <br/>
+    <a href="/projects/create">Create a new Project</a>
+
 @endsection
