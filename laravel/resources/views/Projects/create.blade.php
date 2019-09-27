7e9b954a79a76b662fa3cab569b1860bec22187a
Reese Morgan <ramorgan212@gmail.com>
2019-09-27 01:48:02 -0400

    create an errors include blade.

--- laravel/resources/views/Projects/create.blade.php
+++ laravel/resources/views/Projects/create.blade.php
@@ -1,21 +1,29 @@
 @extends('layout')
 @section('content')
     <h1>Create a new Projects</h1>
-{{-- --}}
+    {{-- --}}
     <form method="POST" action="/projects">
         @csrf
         <div class="field">
             <label class="label" for="title">Project Title</label>
             <div class="control">
-                <input type="text" class="input {{$errors->has('title') ? 'is-danger' : ''}}"
-                       name="title" required value="{{old('title')}}"/>
+                <input
+                        type="text"
+                        class="input {{$errors->has('title') ? 'is-danger' : ''}}"
+                        name="title"
+                        {{--required--}}
+                        value="{{old('title')}}"
+                />
             </div>
         </div>
         <div class="field">
             <label class="label" for="description">Project description</label>
             <div class="control">
-                <textarea class="textarea {{$errors->has('title') ? 'is-danger' : ''}}"
-                          name="description" required>{{old('description')}}</textarea>
+                <textarea
+                        class="textarea {{$errors->has('title') ? 'is-danger' : ''}}"
+                        name="description"
+                        {{--required--}}
+                >{{old('description')}}</textarea>
             </div>
         </div>
         <div class="field">
@@ -23,15 +31,6 @@
                 <button type="submit" class="is-link"> Create Project</button>
             </div>
         </div>
-
-        @if($errors->any())
-            <div class="notification is-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li> {{$error}} </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        @include('errors')
     </form>
 @endsection
