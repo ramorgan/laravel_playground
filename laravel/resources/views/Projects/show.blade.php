24cba7e89e67aa213b0b4f8f27919cd264a00b50
Reese Morgan <ramorgan212@gmail.com>
2019-09-26 23:56:43 -0400

    display a Projects Tasks on the Projects.show page

--- laravel/resources/views/Projects/show.blade.php
+++ laravel/resources/views/Projects/show.blade.php
@@ -1,9 +1,24 @@
 @extends('layout')
 
 @section('content')
-<h1 class="title">{{$project->title}}</h1>
 
-    <div class="content">{{$project->description}}</div>
+    <h1 class="title">{{$project->title}}</h1>
 
-    <p><a href="/projects/{{$project->id}}/edit">Edit</a> </p>
+    <div class="content">
+        {{$project->description}}
+
+        <p>
+            <a href="/projects/{{$project->id}}/edit">Edit</a>
+        </p>
+    </div>
+
+    @if ($project->tasks->count())
+        <div>
+            <ul>
+                @foreach($project->tasks as $task)
+                    <li>{{$task->description}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 @endsection
