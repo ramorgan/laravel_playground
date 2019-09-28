97f4db9f2ef9262a78deef65a3282e623f1aa78b
Reese Morgan <ramorgan212@gmail.com>
2019-09-28 00:10:50 -0400

    add me and the test admin account to the users whom can use Telescope.

--- laravel/app/Providers/TelescopeServiceProvider.php
+++ laravel/app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                //
+                'admin@example.com',
+                'ramorgan212@gmai.com',
             ]);
         });
     }
